b199d1b080235aa407f233593acafe6500067f7e
Mateus Castro <matcastro>
2018-10-12 13:30:10 -0300

    Ajuste no ID da classe Convidado e criação da funcionalidade de salvar convidado.

--- listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.alura.listavip.model.Convidado;
 import br.com.alura.listavip.repository.ConvidadoRepository;
@@ -24,5 +26,12 @@ public class ConvidadoController {
 		Iterable<Convidado> convidados = repository.findAll();
 		model.addAttribute("convidados", convidados);
 		return "listaconvidados";
+	}
+	
+	@RequestMapping(value="/salvar", method=RequestMethod.POST)
+	public String salvar(@RequestParam("nome") String nome, @RequestParam("email") String email, @RequestParam("telefone") String telefone) {
+		Convidado convidado = new Convidado(nome, email, telefone);
+		repository.save(convidado);
+		return "redirect:listaconvidados";
 	}
 }
--- listavip/src/main/java/br/com/alura/listavip/model/Convidado.java
+++ listavip/src/main/java/br/com/alura/listavip/model/Convidado.java
@@ -2,16 +2,28 @@ package br.com.alura.listavip.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name="convidado")
 public class Convidado {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
 	private String telefone;
+	
+	public Convidado() {
+		
+	}
+	
+	public Convidado(String nome, String email, String telefone) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
 	public Long getId() {
 		return id;
 	}
